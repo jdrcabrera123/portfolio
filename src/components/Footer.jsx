ce630c647b1977c54999d7c7f2a27c923c5f76a2
jdrcabrera <jd.rcabrera@gmail.com>
2023-04-12 18:56:28 -0500

    gh added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ export default function Footer() {
   return (
     <>
       <div className="containerFooter">
-      
+      <a href="https://github.com/jdrcabrera123" target="_blank">
+          <i className="bx bxl-github"></i>
+        </a>
         <a
           href="https://www.linkedin.com/in/juan-david-rodriguez-cabrera-021a5326/"
           target="_blank"
