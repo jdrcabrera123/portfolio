5557c442c43ee93e5b3702523647d231536a48c5
jdrcabrera <jd.rcabrera@gmail.com>
2023-04-12 18:54:32 -0500

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,7 @@ export default function Footer() {
   return (
     <>
       <div className="containerFooter">
-        <a href="https://github.com/jdrcabrera123" target="_blank">
-          <i className="bx bxl-github"></i>
-        </a>
+      
         <a
           href="https://www.linkedin.com/in/juan-david-rodriguez-cabrera-021a5326/"
           target="_blank"
