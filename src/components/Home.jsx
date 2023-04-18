5adebdf328835d5014501109d870371b520d4005
jdrcabrera <jd.rcabrera@gmail.com>
2023-04-17 23:25:02 -0500

    style corrections

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,16 +10,21 @@ import { HashLink as Link } from "react-router-hash-link";
 export default function Home() {
   return (
     <>
-    
       <section className="home">
         <p>
-          I love <span>endowing life</span> to my <span>creations</span> and{" "}
-          <span>designs</span>. <span>Programming</span> is the way to connect
-          my <span> ideas</span> with people <span> interaction</span>.
+          I am passionate about bringing my <span>creations </span> and{" "}
+          <span> designs to life</span>, and 
+          <span> programming</span> is the key to making that happen. By using
+          code to connect my <span> ideas</span> with real-world{" "}
+          <span> interaction</span>, I can build dynamic, engaging experiences
+          that resonate with people. Programming allows me to harness the power
+          of technology to create something truly meaningful and impactful.
         </p>
         <div className="gaussianOne"></div>
         <div id="letsTalk" className="letsTalk">
-          <Link to="#contact" smooth>Let's Talk</Link>
+          <Link to="#contact" smooth>
+            Let's Talk
+          </Link>
           {/* {menuLinks.map((link) => {
             return <a href={link.url} key={link.id}>{link.text}</a>;
           })} */}
