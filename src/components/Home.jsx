8539c738b63b4d91d52f553c675506b7c05442d1
jdrcabrera <jd.rcabrera@gmail.com>
2023-04-24 16:45:29 -0500

    new styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,9 +33,37 @@ export default function Home() {
             return <a href={link.url} key={link.id}>{link.text}</a>;
           })} */}
         </div>
+        <div className="custom-shape-divider-bottom-1682370008">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
       </section>
+
       <ProgrammingUxUi />
+      
       <div className="contactSection">
+      <div className="custom-shape-divider-top-1682371163">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            className="shape-fill"
+          ></path>
+        </svg>
+      </div>
         <Contact />
         <ContactForm />
       </div>
