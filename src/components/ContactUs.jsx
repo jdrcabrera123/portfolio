663c83d92f91ddb33620bd3dcf3efd6969a48fe8
jdrcabrera <jd.rcabrera@gmail.com>
2023-04-24 19:42:00 -0500

    form completed

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,7 +1,12 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import { useState } from "react";
 
 export default function ContactUs() {
+  const [fullname, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [requirement, setRequirement] = useState("");
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -16,7 +21,11 @@ export default function ContactUs() {
       )
       .then(
         (result) => {
-          console.log(result.text);
+          alert("I will contact you soon");
+          setFullName("");
+          setEmail("");
+          setMobile("");
+          setRequirement("");
         },
         (error) => {
           console.log(error.text);
@@ -32,6 +41,9 @@ export default function ContactUs() {
         type="text"
         name="user_name"
         placeholder="Your full name"
+        value={fullname}
+        onChange={(e) => setFullName(e.target.value)}
+        required
       />
       {/* <label>Email</label> */}
       <input
@@ -39,11 +51,27 @@ export default function ContactUs() {
         type="email"
         name="user_email"
         placeholder="Your mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
       />
       {/* <label>Mobile</label> */}
-      <input className="inputStyle" type="tel" name="contact_number" placeholder="Your mobile" />
+      <input
+        className="inputStyle"
+        type="tel"
+        name="contact_number"
+        placeholder="Your mobile"
+        value={mobile}
+        onChange={(e) => setMobile(e.target.value)}
+        required
+      />
       {/* <label>Message</label> */}
-      <textarea name="message" placeholder="How could I help you?" />
+      <textarea
+        name="message"
+        placeholder="How could I help you?"
+        value={requirement}
+        onChange={(e) => setRequirement(e.target.value)}
+      />
       <input type="submit" value="Send" />
     </form>
   );
