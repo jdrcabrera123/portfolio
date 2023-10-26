474ff4072b31beed5a53dcc729b425fa84c8d99e
jdrcabrera <jd.rcabrera@gmail.com>
2023-10-25 21:41:05 -0500

    Changing IG links

--- src/api/animationCards.jsx
+++ src/api/animationCards.jsx
@@ -1,106 +1,93 @@
 export const animationCards = [
-    {
-        id: 1,
-        image: 'https://i.ibb.co/94YJS2p/image-11.png',
-        title: "Beach day",
-        description: "PS, AE",
-        url: 'https://www.instagram.com/p/CmKd6aHLh8o/'
-
-    },
-    {
-        image: 'https://i.ibb.co/wdQJMBD/image-12.png',
-        id: 2,
-        title: "The past",
-        description: "Maya, AE, PS",
-        url: 'https://www.instagram.com/p/ClHrb5NL21A/'
-
-    },
-    {
-        image: 'https://i.ibb.co/SnBG48C/image-13.png',
-        id: 3,
-        title: "Tech Food",
-        description: "C4D, AE",
-        url: 'https://www.instagram.com/p/CgxNBNcrBMS/'
-
-    },
-    {
-        image: 'https://i.ibb.co/kJKxZpq/image-14.png',
-        id: 4,
-        title: "Minds",
-        description: "C4D, AE",
-        url: 'https://www.instagram.com/p/Cge8-3CM-Ok/'
-
-    },
-    {
-        image: 'https://i.ibb.co/NrR37Kh/image-15.png',
-        id: 5,
-        title: "Charming Sounds",
-        description: "C4D, AE",
-        url: 'https://www.instagram.com/p/CgXZi4kMQiV/'
-
-    },
-    {
-        image: 'https://i.ibb.co/gTqrH1M/image-16.png',
-        id: 6,
-        title: "Skips",
-        description: "C4D, AE",
-        url: 'https://www.instagram.com/p/CgSIU24tEWn/'
-
-    },
-    {
-        image: 'https://i.ibb.co/BVVSKt9/image-17.png',
-        id: 7,
-        title: "Types goes on",
-        description: "C4D, AE",
-        url: 'https://www.instagram.com/p/Cf9nJrtPT5u/'
-
-    },
-    {
-        image: 'https://i.ibb.co/v3YbKrJ/image-18.png',
-        id: 8,
-        title: "Night, sun, moon",
-        description: "C4D, AE",
-        url: 'https://www.instagram.com/p/Cf2hmMzuRS2/'
-
-    },
-    {
-        image: 'https://i.ibb.co/Hp25Z4K/image-19.png',
-        id: 9,
-        title: "Lila",
-        description: "C4D, AE",
-        url: 'https://www.instagram.com/p/CfZdnGvPjJi/'
-
-    },
-    {
-        image: 'https://i.ibb.co/pznVcY1/image-20.png',
-        id: 10,
-        title: "Please, move on",
-        description: "C4D, AE",
-        url: 'https://www.instagram.com/p/CfIbwkauELz/'
-
-    },
-    {
-        image: 'https://i.ibb.co/W2tMrbq/image-21.png',
-        id: 11,
-        title: "21k",
-        description: "AI, AE",
-        url: 'https://www.instagram.com/p/CehIJ1IOhPW/'
-
-    },
-    {
-        image: 'https://i.ibb.co/Hqb3RFB/image-22.png',
-        id: 12,
-        title: "Franky",
-        description: "AI, AE",
-        url: 'https://www.instagram.com/p/CePWd7vtZ-c/'
-
-    },
-    {
-        image: 'https://i.ibb.co/3rBrtJJ/image-23.png',
-        id: 13,
-        title: "Paradise",
-        description: "PS, AE",
-        url: 'https://www.instagram.com/p/Cd9AXmTLijo/'
-
-    },
-]
+  {
+    id: 1,
+    image: "https://i.ibb.co/94YJS2p/image-11.png",
+    title: "Beach day",
+    description: "PS, AE",
+    url: "https://www.instagram.com/p/CmKd6aHLh8oqZ37mhWFko167cBE2joFM7qqpwE0/",
+  },
+  {
+    image: "https://i.ibb.co/wdQJMBD/image-12.png",
+    id: 2,
+    title: "The past",
+    description: "Maya, AE, PS",
+    url: "https://www.instagram.com/p/ClHrb5NL21A2PDOksv_wPonj6Dcksad3jVsxKM0/",
+  },
+  {
+    image: "https://i.ibb.co/SnBG48C/image-13.png",
+    id: 3,
+    title: "Tech Food",
+    description: "C4D, AE",
+    url: "https://www.instagram.com/p/CgxNBNcrBMSTgzovl32PbHDd6Msd9NZAauhkxQ0/",
+  },
+  {
+    image: "https://i.ibb.co/kJKxZpq/image-14.png",
+    id: 4,
+    title: "Minds",
+    description: "C4D, AE",
+    url: "https://www.instagram.com/p/Cge8-3CM-Ok0digkQdRwmAQHbbrMA_MMHBMkPU0/",
+  },
+  {
+    image: "https://i.ibb.co/NrR37Kh/image-15.png",
+    id: 5,
+    title: "Charming Sounds",
+    description: "C4D, AE",
+    url: "https://www.instagram.com/p/CgXZi4kMQiVle8mTR0DpkDPoUK8h3OA8EI5Qdg0/",
+  },
+  {
+    image: "https://i.ibb.co/gTqrH1M/image-16.png",
+    id: 6,
+    title: "Skips",
+    description: "C4D, AE",
+    url: "https://www.instagram.com/p/CgSIU24tEWnlEgcx4gDQBtHlkLyA2Xq7ctCjFQ0/",
+  },
+  {
+    image: "https://i.ibb.co/BVVSKt9/image-17.png",
+    id: 7,
+    title: "Types goes on",
+    description: "C4D, AE",
+    url: "https://www.instagram.com/p/Cf9nJrtPT5uCfA3B2JUoU4RP1duR3cqXDWJ-Ac0/",
+  },
+  {
+    image: "https://i.ibb.co/v3YbKrJ/image-18.png",
+    id: 8,
+    title: "Night, sun, moon",
+    description: "C4D, AE",
+    url: "https://www.instagram.com/p/Cf2hmMzuRS2rLyjjMBnhQsSFTdMDSbH-Hu8m9I0/",
+  },
+  {
+    image: "https://i.ibb.co/Hp25Z4K/image-19.png",
+    id: 9,
+    title: "Lila",
+    description: "C4D, AE",
+    url: "https://www.instagram.com/p/CfZdnGvPjJiQ-jRxhnQeWfbhfwwjG_KUqbCN100/",
+  },
+  {
+    image: "https://i.ibb.co/pznVcY1/image-20.png",
+    id: 10,
+    title: "Please, move on",
+    description: "C4D, AE",
+    url: "https://www.instagram.com/p/CfIbwkauELzs2K7E1WCc7xZWqfh02P8JAfuFWY0/",
+  },
+  {
+    image: "https://i.ibb.co/W2tMrbq/image-21.png",
+    id: 11,
+    title: "21k",
+    description: "AI, AE",
+    url: "https://www.instagram.com/p/CehIJ1IOhPWTl33o9M2aySgy8SHBNiMwp8wWEY0/",
+  },
+  {
+    image: "https://i.ibb.co/Hqb3RFB/image-22.png",
+    id: 12,
+    title: "Franky",
+    description: "AI, AE",
+    url: "https://www.instagram.com/p/CePWd7vtZ-c0sOLDxpmft7wUdVjMLYYPB3Evyw0/",
+  },
+  {
+    image: "https://i.ibb.co/3rBrtJJ/image-23.png",
+    id: 13,
+    title: "Paradise",
+    description: "PS, AE",
+    url: "https://www.instagram.com/p/Cd9AXmTLijocYrE4lW1b7dUBNkh3Y12rmEsPCk0/",
+  },
+];
